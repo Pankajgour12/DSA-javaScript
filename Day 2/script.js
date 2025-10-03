88d65991cc85bbb632f3dec37d1bb11c3c2a71e4
pankajgour12 <pankajgour5000@gmail.com>
2025-10-03 08:47:45 +0530

    Math.ceil() function

---
+++ Day 2/script.js
@@ -0,0 +1,18 @@
+// Math Functions
+// what is Math function ? 
+// The Math object is a built-in object in JavaScript that provides various mathematical functions and constants.
+// It is not a function or a constructor, but rather a static object that contains properties and methods for performing mathematical operations.
+
+// Example of Math functions    
+
+// math.ceil() - Rounds a number UP to the nearest integer 
+console.log(Math.ceil(4.2)); // Output: 5
+console.log(Math.ceil(-4.2)); // Output: -4 
+console.log(Math.ceil(4.7)); // Output: 5
+
+
+
+
+
+
+
