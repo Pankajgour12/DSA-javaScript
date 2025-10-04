9e1e0ad0bd2dad9c3256a9165ec5ceb9c8b3983a
pankajgour12 <pankajgour5000@gmail.com>
2025-10-04 21:11:28 +0530

    #

--- Day 3/script.js
+++ Day 3/script.js
@@ -22,3 +22,20 @@ let isLeapYear = false;
 isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
 
 console.log(isLeapYear ? year + " is a leap year" : year + " is not a leap year");
+
+
+
+// discounted price
+
+
+let amount = Number(prompt("Enter the original price: "));
+let dis = 0;
+
+if(amount >= 0 && amount <= 5000) dis = 0;
+else if(amount > 5000 && amount <= 7000) dis = 5;
+else if(amount > 7000 && amount <= 9000) dis = 10;
+else if(amount > 9000 ) dis = 20;
+
+else console.log("Invalid amount");
+ 
+console.log("Discounted price is: " + (amount - (amount * dis / 100)));
