4fe681e38f5d6983ab5276f96d8f75f95f962cf7
pankajgour12 <pankajgour5000@gmail.com>
2025-10-07 13:09:36 +0530

    number is prime or not

--- Day 5/script.js
+++ Day 5/script.js
@@ -19,11 +19,28 @@ console.log(`The sum of all odd numbers from 0 to ${n} is: ${oddSum}`); */
 
 
 //Q. Print all the factors of a number
-
+/* 
 let n = Number(prompt("Enter a number: "));
 
 for(let i = 1; i <= n/2; i++) {
     if(n % i === 0) {
         console.log(i);
-    } }
+    } } */
+
+
+        // Q.check if the number is prime or not.
+let n = Number(prompt("Enter a number: "));
+let isPrime = true;
+for(let i = 2; i <= n/2; i++) {
+    if(n % i === 0) {
+        isPrime = false;
+        break;
+    }   }
+if(isPrime) {
+    console.log(`${n} is a prime number`);
+} else {
+    console.log(`${n} is not a prime number`);
+}
+
+
 
