c4e30b4a8f22ffb018d019e335fea3cacced92f1
pankajgour12 <pankajgour5000@gmail.com>
2025-10-16 09:00:54 +0530

    Star Pattern

--- Sheryi Js DSA/Day 8/script.js
+++ Sheryi Js DSA/Day 8/script.js
@@ -1,11 +1,59 @@
-const prompt = require("prompt-sync")()
-
+/* const prompt = require("prompt-sync")()
 const n = prompt("Enter a number ")
-for(let i=1;i<=n;i++){
-temp = 65;
-for(let j=1;j<=i;j++){
-    process.stdout.write(String.fromCharCode(temp)+" ");
-    temp++
+ */
+/* for(let i=1;i<=n;i++){
+    temp = 65;
+    for(let j=1;j<=i;j++){
+            process.stdout.write(String.fromCharCode(temp)+" ");
+            temp++
+        }
+        console.log();
+        }  */
+
+//!  Q.
+
+/* 
+            *
+          * *
+        * * *
+      * * * *
+    * * * * *
+  * * * * * *
+
+*/
+
+/* 
+        for(let i=1;i<=n;i++){
+
+        for(let j=1;j<=n-i ; j++ ){
+            process.stdout.write("  ")
+        }
+        for(let j=1; j<=i;j++){
+            process.stdout.write('* ')
+        }
+        console.log();
+
+        } */
+
+const prompt = require("prompt-sync")();
+const n = Number(prompt("Enter a number "));
+
+
+/* 
+              *       *
+                *   *
+                  *
+                *   *
+              *       *
+*/
+
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n; j++) {
+    if (i == j || i + j === n + 1) {
+      process.stdout.write("* ");
+    } else process.stdout.write("  ");
+  }
+
+  console.log();
 }
-console.log();
-}    
