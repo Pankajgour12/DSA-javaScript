e52e33e3e784648e157255e7399531d92a72f969
pankajgour12 <pankajgour5000@gmail.com>
2025-11-01 17:00:26 +0530

    `Added multiple array operations: sum, max, and min element finder.`

--- Sheryi Js DSA/Array/app.js
+++ Sheryi Js DSA/Array/app.js
@@ -1,4 +1,6 @@
-let prompt = require("prompt-sync")();
+// Q. Sum of all elements in an array
+
+/* let prompt = require("prompt-sync")();
 
 let size = Number(prompt("Enter size of array "));
 
@@ -9,4 +11,50 @@ for (let i = 0; i < size; i++) {
   arr[i] = Number(prompt(`Enter element at index ${i} : `));
   sum += arr[i];
 }
-console.log(`Sum of all elements in array is ${sum}`);
+console.log(`Sum of all elements in array is ${sum}`); */
+
+
+
+// Q. Find maximum element in an array
+
+/* let prompt = require("prompt-sync")();
+let size = Number(prompt("Enter size of array "));
+
+let arr = new Array(size);
+
+
+for (let i = 0; i < size; i++) {
+  arr[i] = Number(prompt(`Enter element at index ${i} : `));
+  }
+let max = arr[0];
+for (let i = 1; i < size; i++) {
+  if (arr[i] > max) { 
+    max = arr[i];
+  }
+}
+console.log(`Maximum element in array is ${max}`);
+ */
+
+
+// Q. Find minimum element in an array
+
+let prompt = require("prompt-sync")();
+let size = Number(prompt("Enter size of array "));
+let arr = new Array(size);
+
+for (let i = 0; i < size; i++) {
+  arr[i] = Number(prompt(`Enter element at index ${i} : `));
+}
+let min = arr[0];
+for (let i = 1; i < size; i++) {
+  if (arr[i] < min) {
+    min = arr[i];
+  }   
+}
+console.log(`Minimum element in array is ${min}`);
+  
+
+
+
+
+
