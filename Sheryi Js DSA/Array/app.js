4a4ec8d7abfa9766bda0511b5f1cce1fb6d9a88b
pankajgour12 <pankajgour5000@gmail.com>
2025-11-03 09:17:35 +0530

    `Added code to reverse an array with extra space`

--- Sheryi Js DSA/Array/app.js
+++ Sheryi Js DSA/Array/app.js
@@ -91,3 +91,21 @@ for (let i = 1; i < arr.length; i++) {
 console.log(`Smallest element in array is ${min}`);
  */
 
+//! Reverse an array with exotra space
+
+let size = Number(prompt("Enter size of array "));
+let arr = new Array(size);
+for (let i = 0; i < arr.length; i++) {
+  arr[i] = Number(prompt(`Enter element at index ${i} : `));
+}
+
+let revArr = new Array(arr.length);
+let j= 0;
+for (let i = arr.length - 1; i >= 0; i--) {
+  revArr[j] = arr[i];
+  j++;
+}
+
+console.log("Reversed Array is :"+revArr);
+ 
+
