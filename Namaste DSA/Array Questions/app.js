7d5677644c89bd69f2a8c88d4269733c17d3fe71
pankajgour12 <pankajgour5000@gmail.com>
2025-11-13 17:28:09 +0530

    Refactor removeDuplicates function and enhance removeElement logic

--- Namaste DSA/Array Questions/app.js
+++ Namaste DSA/Array Questions/app.js
@@ -1,7 +1,7 @@
 
 //! Remove Dupticate in Sorted array 
 
-// Import prompt-sync so we can take input from user in VS Code terminal
+/* // Import prompt-sync so we can take input from user in VS Code terminal
 const prompt = require("prompt-sync")();
 
 // Step 1: Take array input
@@ -17,7 +17,7 @@ console.log("\nOriginal Array:", nums);
 // Step 2: Function to remove duplicates (LeetCode logic)
 function removeDuplicates(nums) {
   // If array is empty, return 0
-  if (nums.length === 0) return 0;
+  if(nums.length === 0) return 0;
 
   // Pointer 'x' will track the last position of unique elements
   let x = 0;
@@ -43,3 +43,46 @@ const k = removeDuplicates(nums);
 // Step 4: Print result
 console.log("\nTotal unique elements:", k);
 console.log("Array after removing duplicates:", nums.slice(0, k));
+
+ */
+
+
+
+//! Remove Element 
+
+const prompt = require("prompt-sync")();
+
+// Step 1: Take array input
+const size = Number(prompt("Enter the size of the array: "));
+const arr = new Array(size);
+
+for (let i = 0; i < size; i++) {
+  arr[i] = Number(prompt(`Enter element ${i + 1}: `));
+}
+
+console.log("\nOriginal Array:", arr);
+const val = Number(prompt("Enter the value to remove: "));
+
+function removeElement(arr, val) {
+  let k = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== val) {
+      arr[k] = arr[i];
+      k++;
+    }
+  }
+
+  return arr.slice(0, k); // <- CLEAN RESULT
+}
+
+let result = removeElement(arr, val);
+console.log("After Removal:", result);
+
+
+
+
+
+
+
+
