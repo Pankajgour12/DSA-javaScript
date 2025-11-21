d4ef680aaee369317062a5ba49c2113dc745dad7
pankajgour12 <pankajgour5000@gmail.com>
2025-11-21 12:46:52 +0530

    Refactor 2D array input handling and improve console output formatting

--- Sheryi Js DSA/2D Array/app.js
+++ Sheryi Js DSA/2D Array/app.js
@@ -1,11 +1,21 @@
 let prompt = require('prompt-sync')()
-let innerArraySize = Number(prompt("Enter the array size"))
+let innerArraySize = Number(prompt("Enter the array size : "))
 
 
 let arr = new Array(innerArraySize)
 
-
 for(let i= 0; i<arr.length; i++){
-    arr[i] = new Array()
+    let size = Number(prompt(`Enter the size of inner array ${i+1} : `))
+    arr[i] = new Array(size)
+    for(let j=0; j<arr[i].length; j++){
+        arr[i][j] = Number(prompt("Enter the value for inner array " + (i+1) + " at index " + j + ": "))
+    }       
 }
-console.log(arr);
+console.log("The 2D Array is: ")
+console.log(arr)
+console.log("The elements of 2D Array are: ")
+for(let i=0; i<arr.length; i++){
+    for(let j=0; j<arr[i].length; j++){
+        console.log("Element at arr[" + i + "][" + j + "] = " + arr[i][j])
+    }
+}       
