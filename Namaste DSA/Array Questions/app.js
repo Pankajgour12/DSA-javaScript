07d7bea156e397d53d2d497a142a694c30d146e8
pankajgour12 <pankajgour5000@gmail.com>
2025-11-23 19:29:01 +0530

    Add function to find the single number using Bitwise XOR

--- Namaste DSA/Array Questions/app.js
+++ Namaste DSA/Array Questions/app.js
@@ -419,7 +419,18 @@ console.log(singleNumber(nums)); */
 
 
 
+//? sec method usin Bitwish XOR 
 
+function singleNumber(nums) {
+  let result = 0;
+  for (let i = 0; i < nums.length; i++) {
+    result ^= nums[i];
+  }
+  return result;
+}
+
+console.log(singleNumber(nums));
+    
 
 
 
