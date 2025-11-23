84e5304d9f996beb2e974b466b1774a2dd312b7e
pankajgour12 <pankajgour5000@gmail.com>
2025-11-23 19:16:50 +0530

    add  Single Number function

--- Namaste DSA/Array Questions/app.js
+++ Namaste DSA/Array Questions/app.js
@@ -351,7 +351,7 @@ console.log("Array after duplicating zeros:", duplicatedArray);
 
 //! Height Checker
 
- const prompt = require("prompt-sync")();
+/*  const prompt = require("prompt-sync")();
 
 // Step 1: Take array input
 const size = Number(prompt("Enter the size of the array: "));
@@ -360,7 +360,7 @@ const nums = new Array(size);
 for (let i = 0; i < size; i++) {
   nums[i] = Number(prompt(`Enter element ${i + 1}: `));
 } 
-console.log("\nOriginal Array:", nums);
+console.log("\nOriginal Array:", nums); */
 
 /* 
  function heightChecker(heights) {
@@ -380,7 +380,44 @@ console.log("Height Mismatch Count:", heightMismatchCount);
  */
 
 
-//! Single Number
+//! Single Number 
+
+const prompt = require("prompt-sync")();
+
+// Step 1: Take array input
+const size = Number(prompt("Enter the size of the array: "));
+const nums = new Array(size);
+
+for (let i = 0; i < size; i++) {
+  nums[i] = Number(prompt(`Enter element ${i + 1}: `));
+} 
+console.log("\nOriginal Array:", nums); 
+
+
+//? first method using Hashmap : it is bad bc use extra memory
+/* function singleNumber(nums) {
+  let hash = {};
+
+  for(let i = 0; i < nums.length ; i++){
+    if(hash[nums[i]] === undefined){
+      hash[nums[i]] = 1;
+    } else{
+      hash[nums[i]]++;
+    
+    }
+  }
+  for(let i = 0 ; i < nums.length ; i++ ){
+    if(hash[nums[i]] === 1){
+      return nums[i];
+    }
+  }
+
+
+}
+
+console.log(singleNumber(nums)); */
+
+
 
 
 
