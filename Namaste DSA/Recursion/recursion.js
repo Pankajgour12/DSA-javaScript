05cef9642329d9d13c4253ce89a04de654b17ac8
pankajgour12 <pankajgour5000@gmail.com>
2025-11-24 10:01:45 +0530

    Revers number to user input

--- Namaste DSA/Recursion/recursion.js
+++ Namaste DSA/Recursion/recursion.js
@@ -1,12 +1,32 @@
 
+// ? Recursion & call Stack
 
-function fun(num) {
-  // code here
+/* function fun(num) {
+
   console.log(num);
   num = num-1;
+ 
   fun(num);
+
 
 
 }
 let a = 5;
-fun(a)
+fun(a) */
+
+
+
+//* Recursion & Call Stack 
+
+const prompt = require('prompt-sync')();
+
+const nums = Number(prompt('Enter A nubmer :-  '));
+
+function fun (nums){
+    if(nums == 0 ) return ;
+    console.log(nums);
+    nums-=1 
+    fun(nums)
+
+}
+fun(nums);
