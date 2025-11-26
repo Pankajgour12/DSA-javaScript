675b108250a937488da8045d3e5844ad9eec2d40
pankajgour12 <pankajgour5000@gmail.com>
2025-11-26 17:54:55 +0530

    Implement array input and recursive sum function for array elements

--- Namaste DSA/Recursion/app.js
+++ Namaste DSA/Recursion/app.js
@@ -2,11 +2,34 @@
 
 const prompt = require('prompt-sync')();
 
-const a = Number(prompt('Enter A nubmer :-  '));
-
- function sum(n){
-    if(n==0) return 0;
-    return n + sum(n-1)
+const size = Number(prompt('Enter a Array Size :-  '));
+const arr = new Array(size);
+for(let i=0; i<size; i++){
+    arr[i] = Number(prompt(`Enter ${i} Element :- `));
+    
+}
+for(let i=0; i<size; i++){
+    console.log(arr[i]);
 }
 
+//! Sum of all elements in an array using recursion
+ function sumArray(arr, size){
+    if(size <=0) return 0;
+    return arr[size-1] + sumArray(arr, size-1)
+}
+console.log("Sum of all elements in an array is :- ", sumArray(arr, size));
+
+
+
+
+
+
+ /* function sum(n){
+    if(n==0) return 0;
+    return n + sum(n-1)
+} 
+
 console.log("Sum of first n natural numbers is :- ", sum(a));
+*/
+
+
