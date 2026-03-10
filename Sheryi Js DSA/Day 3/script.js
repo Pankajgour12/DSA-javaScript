6f9ebe1238eda555d660bc52853eaede8e86e573
pankajgour12 <pankajgour5000@gmail.com>
2026-03-10 10:52:58 +0530

    Comment out unused code sections for clarity and organization

--- Sheryi Js DSA/Day 3/script.js
+++ Sheryi Js DSA/Day 3/script.js
@@ -16,19 +16,19 @@ console.log(isLeapYear ? year + " is a leap year" : year + " is not a leap year"
  */
 // second method
 
-let year = Number(prompt("Enter a year: "));
+/* let year = Number(prompt("Enter a year: "));
 let isLeapYear = false;
 
 isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
 
 console.log(isLeapYear ? year + " is a leap year" : year + " is not a leap year");
-
+ */
 
 
 // discounted price
 
 
-let amount = Number(prompt("Enter the original price: "));
+/* let amount = Number(prompt("Enter the original price: "));
 let dis = 0;
 
 if(amount >= 0 && amount <= 5000) dis = 0;
@@ -39,19 +39,29 @@ else if(amount > 9000 ) dis = 20;
 else console.log("Invalid amount");
  
 console.log("Discounted price is: " + (amount - (amount * dis / 100)));
-
+ */
 
 // third method using ternary operator
 
-
+/* 
 let amount1 = Number(prompt("Enter the original price: "));
-let dis1 = (amount1 >= 0 && amount1 <= 5000) ? 0 : (amount1 > 5000 && amount1 <= 7000) ? 5 : (amount1 > 7000 && amount1 <= 9000) ? 10 : (amount1 > 9000) ? 20 : -1;
+let dis1 = (amount1 >= 0 && amount1 <= 5000) ? 0 :
+ (amount1 > 5000 && amount1 <= 7000) ? 5 :
+  (amount1 > 7000 && amount1 <= 9000) ? 10 : 
+  (amount1 > 9000) ? 20 : -1;
 if(dis1 === -1) console.log("Invalid amount");
 else console.log("Discounted price is: " + (amount1 - (amount1 * dis1 / 100)));
-
+ */
 
 // check if a number is positive, negative or zero
 
+let b = document.querySelector("body");
+
 let num = Number(prompt("Enter a number: "));
-let result = (num > 0) ? "Positive" : (num < 0) ? "Negative" : "Zero";
-console.log(result);
+let result = (num > 0) ? "Positive" :
+             (num < 0) ? "Negative" : "Zero";
+
+let p = document.createElement("p");
+p.textContent = result;
+
+b.appendChild(p);
