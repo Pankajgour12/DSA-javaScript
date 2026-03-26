8e5ff8fad5da5387c60e3bac378d0dacdef7cd43
Pankaj Gour <165242486+Pankajgour12@users.noreply.github.com>
2026-03-26 15:00:11 +0530

    Update tempCodeRunnerFile.js

--- Sheryi Js DSA/Day 8/tempCodeRunnerFile.js
+++ Sheryi Js DSA/Day 8/tempCodeRunnerFile.js
@@ -7,3 +7,18 @@
 
   console.log();
 }
+
+
+
+
+ for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= (n*2)-1; j++) {
+    if (i == j || i + j === n÷1) {
+      process.stdout.write("* ");
+    } else process.stdout.write("  ");
+  }
+
+  console.log();
+}
+
+
